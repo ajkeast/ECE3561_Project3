216c50012f7b439344be48872f38f523f07723ea
E-Atak <94143796+E-Atak@users.noreply.github.com>
2021-12-09 14:31:41 -0500

    Add files via upload

--- Proj3/top.vhd
+++ Proj3/top.vhd
@@ -30,30 +30,95 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity top is
-    Port ( M1 : in  STD_LOGIC;
-           M2 : in  STD_LOGIC;
-           M3 : in  STD_LOGIC;
-           M4 : in  STD_LOGIC;
-           N1 : in  STD_LOGIC;
-           N2 : in  STD_LOGIC;
-           N3 : in  STD_LOGIC;
-           N4 : in  STD_LOGIC;
+    Port ( M : in STD_LOGIC_VECTOR (4 downto 1);
+           N : in  STD_LOGIC_VECTOR (4 downto 1);
            START : in  STD_LOGIC;
-           R1 : out  STD_LOGIC;
-           R2 : out  STD_LOGIC;
-           R3 : out  STD_LOGIC;
-           R4 : out  STD_LOGIC;
-           R5 : out  STD_LOGIC;
-           R6 : out  STD_LOGIC;
-           R7 : out  STD_LOGIC;
-           R8 : out  STD_LOGIC;
+			  CLK : in STD_LOGIC;
+           R : out  STD_LOGIC_VECTOR (8 downto 1);
            DONE : out  STD_LOGIC);
 end top;
 
 architecture Behavioral of top is
+signal QM: STD_LOGIC_VECTOR (3 downto 0);
+signal QN: STD_LOGIC; 
+signal Aout: STD_LOGIC_VECTOR (3 downto 0);
+signal Cout: STD_LOGIC_VECTOR (3 downto 0);
+
+
+
+component shift_4bit			--Multiplicand (M)
+   Port ( clk : in  STD_LOGIC;
+           clr : in  STD_LOGIC;
+           sel : in  STD_LOGIC_VECTOR (1 downto 0);
+           d : in  STD_LOGIC_VECTOR (3 downto 0);
+           q : inout  STD_LOGIC_VECTOR (3 downto 0);
+           sri : in  STD_LOGIC;
+           sli : in  STD_LOGIC);
+	end component;
+		
+		
+component shift_9bit				-- 9 bit shift register
+	Port (  clk : in  STD_LOGIC;
+           clr : in  STD_LOGIC;
+           sel : in  STD_LOGIC_VECTOR (1 downto 0);
+           d : in  STD_LOGIC_VECTOR (8 downto 0);
+           q : inout  STD_LOGIC_VECTOR (8 downto 0);
+           sri : in  STD_LOGIC;
+           sli : in  STD_LOGIC);
+	end component;
+		
+		
+component adder					-- Adder
+	Port (   X : in  STD_LOGIC_VECTOR (4 downto 1);
+				Y : in  STD_LOGIC_VECTOR (4 downto 1);
+				C1 : in  STD_LOGIC;
+				S : out  STD_LOGIC_VECTOR (4 downto 1);
+				C5 : out  STD_LOGIC);
+		end component;
+
+component counter 				-- Counter
+	Port (  CLR : in  STD_LOGIC;
+           CLK : in  STD_LOGIC;
+			  ENT : in	STD_LOGIC;
+           Q : out  STD_LOGIC_VECTOR (3 downto 0));
+	end component;
+			 
+component sys_controller		-- System Controller
+	Port (  clk : in  STD_LOGIC;
+           start : in  STD_LOGIC;
+           LSB : in  STD_LOGIC;
+           C4 : in  STD_LOGIC;
+           S1 : out  STD_LOGIC_VECTOR (1 downto 0);
+           S2 : out  STD_LOGIC_VECTOR (1 downto 0);
+           S3 : out  STD_LOGIC_VECTOR (1 downto 0);
+           clr_res : out  STD_LOGIC;
+           clr_cntr : out  STD_LOGIC;
+           en_cntr : out  STD_LOGIC;
+           DONE : out  STD_LOGIC);
+	end component;
+
+
 
 begin
 
+C: sys_controller	
+					port map(CLK => clk, START => start, QN => LSB, C4 => C4, S1 => S1, S2 => S2, S3 => S3, clr_res => clr_res, clr_cntr => clr_cntr, en_cntr => en_cntr, DONE => DONE)  
+
+T: counter
+					port map(Clk => CLK, en_cntr => ENT, clr_cntr => CLR, C4 => Q(2))
+					
+A: adder
+				   port map(QM => X, R(7 downto 4) => Y, Aout => S, '0' => C1)
+
+M: shift_4bit
+					port map(CLK => clk, S1 => sel, M => d, QM => q, '1' => clr, '0' => sri, '0' => sli)
+
+N: shift_4bit
+					port map(CLK => clk, S2 => sel, N => d, QN => q(0), '1' => clr, '0' => sri, '0' => sli)
+
+Reg: shift_9bit 
+					port map(CLK => clk, clr_res => clr, S3 => sel, Aout => d(7 downto 4), R(9 downto 1) => q(7 downto 0), C5 => d(8), '0' => sri, '0' = sli)
+					
+--Product <= QS(7 downto 0);
 
 end Behavioral;
-

9245db47c6681766b7ad7cfa4fc49c762cbd3585
ajkeast <94143736+ajkeast@users.noreply.github.com>
2021-12-09 12:35:58 -0500

    Add files via upload

---
+++ shift_9bit.vhd
@@ -0,0 +1,79 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    17:00:10 12/07/2021 
+-- Design Name: 
+-- Module Name:    shift_9bit - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity shift_9bit is
+    Port ( clk : in  STD_LOGIC;
+           clr : in  STD_LOGIC;
+           sel : in  STD_LOGIC_VECTOR (1 downto 0);
+           d : in  STD_LOGIC_VECTOR (8 downto 0);
+           q : inout  STD_LOGIC_VECTOR (8 downto 0);
+           sri : in  STD_LOGIC;
+           sli : in  STD_LOGIC);
+end shift_9bit;
+
+architecture Behavioral of shift_9bit is
+
+begin
+process(clk,sri)
+	begin
+		if (CLR='0')then
+			Q <= "000000000";
+		elsif clk'event and clk='1' then --Rising edge triggered
+		if (SEL="11") then  -- Load data
+			Q<=D;
+		elsif(SEL="00") then -- Hold data
+			Q<=Q;
+		elsif(SEL="01")then --Shift Right Operation
+			Q(8)<= SRI;
+			Q(7)<= Q(8);
+			Q(6)<= Q(7);
+			Q(5)<= Q(6);
+			Q(4)<= Q(5);
+			Q(3)<= Q(4);
+			Q(2)<= Q(3);
+			Q(1)<= Q(2);
+			Q(0)<= Q(1);
+		elsif(SEL="10")then --Shift Left Operation
+			Q(0)<= SLI;
+			Q(1)<= Q(0);
+			Q(2)<= Q(1);
+			Q(3)<= Q(2);
+			Q(4)<= Q(3);
+			Q(5)<= Q(4);
+			Q(6)<= Q(5);
+			Q(7)<= Q(6);
+			Q(8)<= Q(7);	
+			end if;
+		end if;
+end process;
+
+end Behavioral;
+
---
+++ shift_9bit_test.vhd
@@ -0,0 +1,115 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   17:07:32 12/07/2021
+-- Design Name:   
+-- Module Name:   U:/ECE3561/Proj3/shift_9bit_test.vhd
+-- Project Name:  Proj3
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: shift_9bit
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY shift_9bit_test IS
+END shift_9bit_test;
+ 
+ARCHITECTURE behavior OF shift_9bit_test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT shift_9bit
+    PORT(
+         clk : IN  std_logic;
+         clr : IN  std_logic;
+         sel : IN  std_logic_vector(1 downto 0);
+         d : IN  std_logic_vector(8 downto 0);
+         q : INOUT  std_logic_vector(8 downto 0);
+         sri : IN  std_logic;
+         sli : IN  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal clr : std_logic := '0';
+   signal sel : std_logic_vector(1 downto 0) := (others => '0');
+   signal d : std_logic_vector(8 downto 0) := (others => '0');
+   signal sri : std_logic := '0';
+   signal sli : std_logic := '0';
+
+	--BiDirs
+   signal q : std_logic_vector(8 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: shift_9bit PORT MAP (
+          clk => clk,
+          clr => clr,
+          sel => sel,
+          d => d,
+          q => q,
+          sri => sri,
+          sli => sli
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      clr<='0';
+			wait for 10 ns;
+			clr<='1';
+			sel<="11";
+			d<="101010101";
+			wait for 50 ns;
+			sel<="01";
+			SRI <='0';
+			wait for 100 ns;
+			clr<='0';
+			wait for 10 ns;
+			clr<='1';
+			sel<="11";
+			d<="101010101";
+			wait for 50 ns;
+			sel<="10";
+			SLI <='0';
+		  wait for 100 ns;
+   end process;
+
+END;
